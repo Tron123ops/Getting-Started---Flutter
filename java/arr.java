9de93e3b43a456ec82cc1bfd6c4c81c9c3999bb5
Kadhir S <kadhir812128@gmail.com>
2024-09-11 18:20:58 +0530

    first commit

---
+++ java/arr.java
@@ -0,0 +1,8 @@
+class demo{
+    public static void main(String[] args) {
+        String[] arr ={"volvo","ferrari","Audi"};
+        arr[1] = "tata";
+        System.out.println(arr[]);
+
+    }
+}
